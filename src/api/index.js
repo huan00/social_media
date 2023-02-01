fe3b44f7b1e1f9f8a98249661c35c6a548f36f85
huan00 <96999144+huan00@users.noreply.github.com>
2023-02-01 10:39:30 -0500

    change hosting to render

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://huan-social-app.herokuapp.com/'
+  baseURL: 'https://socialmedia-werh.onrender.com'
   // baseURL: 'http://localhost:3003'
 })
 
